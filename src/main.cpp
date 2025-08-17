b3391774d1a751500c0d55295dbb22b62bc1cd74
matty <mattreed1890@gmail.com>
2025-08-17 07:02:18 -0500

    add duck

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main() {
   start();
   createScene();
-  addModel("resources/cup/scene.gltf", glm::vec3{0.0f, -1.0f, 0.0f},
-           glm::vec3{1.0f}, 0.0f, 0.5f);
+  addModel("resources/rubber_duck/scene.gltf", glm::vec3{0.0f, -1.0f, 0.0f},
+           glm::vec3{1.0f}, 0.0f, 1.f);
   run();
 }
